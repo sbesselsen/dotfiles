a84874122276b9a99c5aae51b56b9a99f107e661
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2024-07-17 15:42:15 +0200

    Uncomment some lines

--- vm/arch.sh
+++ vm/arch.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 DIR="$(dirname $0)"
 
-# sudo pacman -Syu
-# sudo pacman -S neovim zsh git github-cli btop bat php rustup starship rsync gitui
-#
-# gh auth status | grep "Logged in" || gh auth login
+sudo pacman -Syu
+sudo pacman -S neovim zsh git github-cli btop bat php rustup starship rsync gitui
+
+gh auth status | grep "Logged in" || gh auth login
 
 "$DIR/../install/gitconfig.sh"
 "$DIR/../install/themes.sh"

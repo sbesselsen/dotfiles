cb2dd6b6fd58ae2bbb1b2abb52990030e26f8d1f
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2024-07-23 17:25:13 +0200

    Add kubie

--- install/config.sh
+++ install/config.sh
@@ -10,3 +10,7 @@ ln -s "$FULLDIR/../config/k9s/config.yaml" ~/.config/k9s/config.yaml
 rm ~/.zshrc
 ln -s "$FULLDIR/../config/zshrc.sh" ~/.zshrc
 
+[ ! -e ~/.kube/kubie.yaml ] && ln -s "$FULLDIR/../config/kubie/config.yaml" ~/.kube/kubie.yaml
+# let kubie see the k3s config file
+mkdir -p ~/.kube/configs
+[ ! -e ~/.kube/configs/k3s.yaml ] && ln -s /etc/rancher/k3s/k3s.yaml ~/.kube/configs/k3s.yaml

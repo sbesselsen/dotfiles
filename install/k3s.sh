ff4fcb95418796c3fb3e555c5fc972f9e5248149
Sebastiaan Besselsen <sebastiaan.besselsen@gmail.com>
2024-07-23 17:06:56 +0200

    Add k3s

--- install/k3s.sh
+++ install/k3s.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+DIR="$(dirname $0)"
+FULLDIR="$(realpath $DIR)"
+
+sudo mkdir -p /etc/rancher/k3s
+[ ! -e /etc/rancher/k3s/config.yaml ] && sudo ln -s "$FULLDIR/../config/k3s/config.yaml" /etc/rancher/k3s/config.yaml 
 curl -sfL https://get.k3s.io | sh -
 
+sudo chmod a+r /etc/rancher/k3s/k3s.yaml
